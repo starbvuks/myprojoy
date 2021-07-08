d2134b0b56b5823d5cbd1e903cdcefbda5c1e3a7
starbvuks <starbvuks@gmail.com>
2021-07-08 23:08:01 +0530

    arrows

--- projoy_v1_demo/v1prj/src/components/Home/Home.jsx
+++ projoy_v1_demo/v1prj/src/components/Home/Home.jsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import Middle from "./MiddleContent/Middle.jsx";
 import NavbarLogo from "./NavLogo/NavbarLogo.jsx";
 
+import rightArrow from "../../assets/images/iconpack-projoy/3.png";
+import leftArrow from "../../assets/images/iconpack-projoy/4.png";
+
 function Home() {
   return (
     <Main>
@@ -10,6 +13,10 @@ function Home() {
         <NavbarLogo />
       </Top>
       <Middle />
+      <MiddleIcons>
+        <ArrowLeft src={leftArrow} alt="left-arrow" />
+        <ArrowRight src={rightArrow} alt="right-arrow" />
+      </MiddleIcons>
     </Main>
   );
 }
@@ -25,4 +32,20 @@ const Top = styled.div`
   position: absolute;
   height: 50%;
   top: 0;
+`;
+
+const MiddleIcons = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  margin: 370px 0 0 0;
+`;
+
+const ArrowRight = styled.img`
+  width: 50px;
+`;
+
+const ArrowLeft = styled.img`
+  width: 50px;
 `;
