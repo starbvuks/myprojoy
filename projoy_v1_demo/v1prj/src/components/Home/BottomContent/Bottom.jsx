c270988358bdd82fe89e6298430fc572d66dbad6
starbvuks <starbvuks@gmail.com>
2021-07-09 00:20:40 +0530

    bottom-text

--- projoy_v1_demo/v1prj/src/components/Home/BottomContent/Bottom.jsx
+++ projoy_v1_demo/v1prj/src/components/Home/BottomContent/Bottom.jsx
@@ -6,6 +6,11 @@ import {
   BlueBubble,
   OrangeBubble,
   GreenBubble,
+  Blue,
+  Orange,
+  Green,
+  Span,
+  Only,
 } from "./stylesBottom.js";
 
 import blueBubble from "../../../assets/images/iconpack-projoy/5.png";
@@ -16,9 +21,21 @@ function Bottom() {
   return (
     <Main>
       <Content>
-        <BlueBubble src={blueBubble} alt="blue" />
-        <OrangeBubble src={orangeBubble} alt="orange" />
-        <GreenBubble src={greenBubble} alt="green" />
+        <Blue>
+          <BlueBubble src={blueBubble} alt="blue" />
+          <Span>happy individual</Span>
+          <Only style={{color: "#C4E0E6"}}>ONLY</Only>
+        </Blue>
+        <Orange>
+          <OrangeBubble src={orangeBubble} alt="orange" />
+          <Span>happy family</Span>
+          <Only style={{color: "#FAE0BE"}}>SAVES</Only>
+        </Orange>
+        <Green>
+          <GreenBubble src={greenBubble} alt="green" />
+          <Span>super family</Span>
+          <Only style={{color: "#D6F3E8"}}>SAVES</Only>
+        </Green>
       </Content>
     </Main>
   );
--- projoy_v1_demo/v1prj/src/components/Home/BottomContent/stylesBottom.js
+++ projoy_v1_demo/v1prj/src/components/Home/BottomContent/stylesBottom.js
@@ -7,18 +7,55 @@ export const Main = styled.div`
 export const Content = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-top: 475px;
+  margin-top: 75vh;
   width: 100%;
 `;
 
+export const Blue = styled.div`
+  position: relative;
+`;
+
+export const Orange = styled.div`
+  position: relative;
+`;
+
+export const Green = styled.div`
+  position: relative;
+`;
+
+export const Span = styled.span`
+  position: absolute;
+  font-family: more_sugarregular;
+  font-size: 3vh;
+  text-align: center;
+  padding: 0 35px 0 0;
+  color: #f2f2f2;
+  line-height: 1.2;
+  z-index: 999;
+  top: 28px;
+  left: 25px;
+`;
+
+export const Only = styled.span`
+  position: absolute;
+  font-weight: bold;
+  font-family: "Fredoka One", cursive;
+  letter-spacing: 1px;
+  font-size: 10px;
+  text-align: center;
+  z-index: 999;
+  top: 52%;
+  left: 38%;
+`;
+
 export const BlueBubble = styled.img`
-  width: 33%;
+  width: 100%;
 `;
 
 export const OrangeBubble = styled.img`
-  width: 33%;
+  width: 100%;
 `;
 
 export const GreenBubble = styled.img`
-  width: 33%;
+  width: 100%;
 `;
