c0f2b5643a337229a6ce901a65cdfa6cf6147387
starbvuks <starbvuks@gmail.com>
2021-07-12 19:42:33 +0530

    api-testing

--- projoy_v1_demo/v1prj/src/components/Packages/ProductCard/ProductCard.jsx
+++ projoy_v1_demo/v1prj/src/components/Packages/ProductCard/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import {
   Main,
@@ -13,8 +13,30 @@ import {
 } from "./stylesProductCard.js";
 
 import cart from "../../../assets/images/iconpack-projoy/2.png";
+import {ConsoleSqlOutlined} from "@ant-design/icons";
+
+const DATA_API = `data.json`;
 
 function ProductCard(props) {
+  const [input, setInput] = useState([]);
+
+  useEffect(() => {
+    fetch(DATA_API, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setInput(...data);
+        console.log(input);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <Main>
@@ -24,6 +46,19 @@ function ProductCard(props) {
           <ProductDetails>
             <ProductName style={{color: props.zerosugColor}}>
               {props.zerosug}
+            </ProductName>
+            <AddToCart>
+              <AddText>Add To Cart: </AddText>
+              <ShoppingTwoToneIcon twoToneColor="#5d98a6" />
+            </AddToCart>
+          </ProductDetails>
+        </ProductSection>
+        <ProductSection>
+          <ProductImg src={input.product_img} />
+          <BackEllipse src={input.ellipse} />
+          <ProductDetails>
+            <ProductName style={{color: props.zerosugColor}}>
+              {input.name}
             </ProductName>
             <AddToCart>
               <AddText>Add To Cart: </AddText>
